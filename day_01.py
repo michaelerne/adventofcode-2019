b8188f787564231ae0b6674cc621183272a97e4a
Michael Erne <michael.erne@gmail.com>
2019-12-01 08:22:29 +0100

    add day 1

---
+++ day_01.py
@@ -0,0 +1,37 @@
+from functools import partial
+from os.path import basename, splitext
+from typing import List
+
+from lib import solve as solve_
+
+DAY: int = int(splitext(basename(__file__))[0].split('_')[1])
+solve = partial(solve_, DAY)
+
+
+def fuel(weight: int) -> int:
+    return weight // 3 - 2
+
+
+def part_a(data: List[int]) -> int:
+    return sum([fuel(module) for module in data])
+
+
+def fuel_rec(weight: int) -> int:
+    additional_fuel = fuel(weight)
+    if additional_fuel <= 0:
+        return 0
+    return additional_fuel + fuel_rec(additional_fuel)
+
+
+def part_b(data: List[int]) -> int:
+    return sum([fuel_rec(module) for module in data])
+
+
+def parse(data: str) -> List[int]:
+    return [int(x) for x in data.split('\n')]
+
+
+if __name__ == "__main__":
+    solve(part_a, parse)
+
+    solve(part_b, parse)
