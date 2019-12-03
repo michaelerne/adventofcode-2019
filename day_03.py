c416bf96f0985825fbafcb45fda0d0e1981b76db
Michael Erne <michael.erne@gmail.com>
2019-12-03 07:25:22 +0100

    extract coord change function

--- day_03.py
+++ day_03.py
@@ -8,15 +8,19 @@ from lib import solve
 DAY: int = int(splitext(basename(__file__))[0].split('_')[1])
 SOLVE = partial(solve, DAY)
 
-Coordinate = Tuple[int, int]
-Wire = List[Coordinate]
+Coordinate = int
+CoordinatePair = Tuple[Coordinate, Coordinate]
+Direction = str
+MovementAmount = int
+DirectionChange = Tuple[Direction, MovementAmount]
+Wire = List[CoordinatePair]
 
 
 def part_a(data: List[Wire]) -> int:
     wire_a: Wire = data[0]
     wire_b: Wire = data[1]
 
-    crossings: Set[Coordinate] = set(wire_a).intersection(wire_b)
+    crossings: Set[CoordinatePair] = set(wire_a).intersection(wire_b)
 
     distances: List[int] = [abs(x) + abs(y) for x, y in crossings if x + y != 0]
 
@@ -35,6 +39,26 @@ def part_b(data: List[Wire]) -> int:
     return min(steps_to_crossing)
 
 
+def get_coordinate_change_for_direction(direction: Direction) -> CoordinatePair:
+    if direction == 'R':
+        x_change = 1
+        y_change = 0
+    elif direction == 'L':
+        x_change = -1
+        y_change = 0
+    elif direction == 'U':
+        x_change = 0
+        y_change = 1
+    elif direction == 'D':
+        x_change = 0
+        y_change = -1
+    else:
+        print(f"found unknown direction: [{direction}]")
+        sys.exit(0)
+
+    return x_change, y_change
+
+
 def parse_wire(data: str) -> Wire:
     direction_changes: List[str] = data.split(',')
 
@@ -47,21 +71,7 @@ def parse_wire(data: str) -> Wire:
         direction = direction_change[0]
         movement_amount = int(direction_change[1:])
 
-        if direction == 'R':
-            x_change = 1
-            y_change = 0
-        elif direction == 'L':
-            x_change = -1
-            y_change = 0
-        elif direction == 'U':
-            x_change = 0
-            y_change = 1
-        elif direction == 'D':
-            x_change = 0
-            y_change = -1
-        else:
-            print(f"found unknown direction: [{direction}]")
-            sys.exit(0)
+        x_change, y_change = get_coordinate_change_for_direction(direction)
 
         for _ in range(0, movement_amount):
             x_coord += x_change
